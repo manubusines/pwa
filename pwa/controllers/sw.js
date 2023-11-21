8094918ba49b0e5820dc55ddc381a7637caa3bc0
manubusines <151655263+manubusines@users.noreply.github.com>
2023-11-21 21:51:42 +0100

    Add files via upload

---
+++ pwa/controllers/sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = 'v1_cache_MasterWeb',
+  urlsToCache = [
+    './',
+    'https://fonts.googleapis.com/css?family=Raleway:400,700',
+    'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
+    'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
+    'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
+    '../views/style.css',
+    '../models/script.js',
+    '../assets/imagen.png',
+    '../assets/favicon.png'
+  ]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
+
+
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      .then(() => self.clients.claim())
+  )
+})
+
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          return res
+        }
+        return fetch(e.request)
+      })
+  )
+})
+
+
